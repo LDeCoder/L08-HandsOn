27ca9108f028ea9178abf4184815dc93cd61ef3a
LoriDeCoder <ldavis1005@gmail.com>
2018-07-13 16:43:09 -0500

    Got everything but toggleClass
    After submit, will add solution under different folder names - for quick reference.

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("header").hover(function(){
+        $(this).css("color","black");
+    }, function(){
+        $(this).css("color", "green");
+    });
+
+});
+
+
+$(document).ready(function(){
+    $("li").dblclick(function(){
+        $(this).hide();
+    });
+});
+
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).text("jQuery is a fast, JavaScript library that makes many tasks easier and simpler to accomplish. A JavaScript library contains pre-written JavaScript which makes developing applications a bit easier for the developer. This means jQuery is not a language, but an extension of JavaScript. It takes many lines of JavaScript code, which we would have had to write ourselves before jQuery, and compresses it.");
+    });
+});
+
+// this crazy thing won't toggle!! 
+
+$(document).ready(function(){
+    $("input").keydown(function(){
+        $(this).css("color", "red");
+    });
+});
+
+// Enter correct toggle syntax below:
+
+
